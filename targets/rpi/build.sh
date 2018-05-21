bded037f334168fdb3bdc69941ce3d66b8200fe2
Michael Rüttgers <michael@ruettgers.eu>
2018-05-21 22:16:08 +0200

    Switched to Node LTS and arm32v7

--- targets/rpi/build.sh
+++ targets/rpi/build.sh
@@ -2,5 +2,5 @@
 
 set -xe
 curl -sSL -o package.json https://github.com/node-red/node-red-docker/raw/master/package.json
-docker build -t mruettgers/rpi-nodered .
+docker build --squash -t mruettgers/rpi-nodered .
 [ ! -f package.json ] || rm package.json
